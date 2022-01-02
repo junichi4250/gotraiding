055c4ff5b256a68e3b72a0a501973589e6a93cf1
junichi4250 <jkky0225@gmail.com>
2022-01-02 14:12:11 +0900

    add:presentation

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.17
 
 require (
 	github.com/gorilla/websocket v1.4.2
+	github.com/mattn/go-sqlite3 v1.14.10
 	gopkg.in/ini.v1 v1.66.2 // indirect
 )
--- main.go
+++ main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"fmt"
+	"gotraiding/app/controllers"
 	"gotraiding/config"
-	"gotraiding/utils"
-	"log"
 )
 
 func main() {
-	fmt.Println(config.Config.ApiKey)
 	fmt.Println(config.Config.LogFile)
-
-	utils.LoggingSetting(config.Config.LogFile)
-	log.Println("test")
+	controllers.StreamIngestionData()
+	controllers.StartWebServer()
 }
